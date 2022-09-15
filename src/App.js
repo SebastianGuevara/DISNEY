f88ba7c4f611ecdc0a46382e781a3f6717bd5579
SebastianGuevara <sebastianguve@unisabana.edu.co>
2022-09-15 00:21:50 -0500

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import Headera from './Components/Headera';
+import Home from './Components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Headera/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
